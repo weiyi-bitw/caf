0ba5e9107c12f05330ac8736c686866751e8ab4a
weiyi_silico2 <wc2302@columbia.edu>
2011-11-16 00:27:18 -0500

    modify qCaf

--- qCaf.sh
+++ qCaf.sh
@@ -4,5 +4,7 @@
 #$ -S /bin/bash
 #$ -t 1-700
 export CONF=$1
+export JOBID=$2
+export BREAKPOINT=$3
 cd /home/weiyi/javaworks/caf
-/usr/java/jdk1.6.0_16/bin/java -Xmx2000M -DSGE_TASK_LAST=$SGE_TASK_LAST -DSGE_TASK_ID=$SGE_TASK_ID -DJOB_ID=$JOB_ID -cp ./dist/caf-0.2.jar caf.CorrAttractorFinder $CONF
+/usr/java/jdk1.6.0_16/bin/java -Xmx2000M -DSGE_TASK_LAST=$SGE_TASK_LAST -DSGE_TASK_ID=$SGE_TASK_ID -DJOB_ID=$JOB_ID -cp ./dist/caf-0.2.jar caf.CorrAttractorFinder $CONF $JOBID $BREAKPOINT
