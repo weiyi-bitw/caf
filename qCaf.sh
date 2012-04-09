0a1702324a0b571011a010ddc100b16dff19bfd3
weiyi_silico2 <wc2302@columbia.edu>
2012-04-09 18:11:14 -0400

    qCaf and confCAF

--- qCaf.sh
+++ qCaf.sh
@@ -2,9 +2,11 @@
 #$ -j y
 #$ -cwd
 #$ -S /bin/bash
-#$ -t 1-700
+#$ -t 1-600
 export CONF=$1
-export JOBID=$2
-export BREAKPOINT=$3
+export COMMAND=$2
+export JOBID=$3 # JOBID for debugging mode / attractor folder for MRC mode
+export BREAKPOINT=$4 # break point for debugging mode / minsize for MRC mode
+export OVLPTH=$5 # overlap threshold for MRC mode
 cd /home/weiyi/javaworks/caf
-/usr/java/jdk1.6.0_16/bin/java -Xmx2000M -DSGE_TASK_LAST=$SGE_TASK_LAST -DSGE_TASK_ID=$SGE_TASK_ID -DJOB_ID=$JOB_ID -cp ./dist/caf-0.2.jar caf.CorrAttractorFinder $CONF $JOBID $BREAKPOINT
+/usr/java/jdk1.6.0_16/bin/java -Xmx2000M -DSGE_TASK_LAST=$SGE_TASK_LAST -DSGE_TASK_ID=$SGE_TASK_ID -DJOB_ID=$JOB_ID -cp ./dist/caf-0.2.jar caf.CorrAttractorFinder $CONF $COMMAND $JOBID $BREAKPOINT $OVLPTH
