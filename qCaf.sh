9bdbb3c4c1cb47da6a15ef9ca2055efc08846bd3
Weiyi Cheng <ninpy.weiyi@gmail.com>
2012-05-12 13:14:33 -0400

    more info in CAF

--- qCaf.sh
+++ qCaf.sh
@@ -2,9 +2,11 @@
 #$ -j y
 #$ -cwd
 #$ -S /bin/bash
-#$ -t 1-7000
+#$ -t 1-1000
 export CONF=$1
-export JOBID=$2
-export BREAKPOINT=$3
-cd /home/weiyi/workspace/javaworks/caf
-java -Xmx2000M -DSGE_TASK_LAST=7000 -DSGE_TASK_ID=460 -DJOB_ID=101 -cp ./dist/caf-0.2.jar caf.CorrAttractorFinder $CONF $JOBID $BREAKPOINT
+export COMMAND=$2
+export JOBID=$3 # JOBID for debugging mode / attractor folder for MRC mode
+export BREAKPOINT=$4 # break point for debugging mode / minsize for MRC mode
+export OVLPTH=$5 # overlap threshold for MRC mode
+cd /home/weiyi/javaworks/caf
+/usr/java/jdk1.6.0_16/bin/java -Xmx2000M -DSGE_TASK_LAST=$SGE_TASK_LAST -DSGE_TASK_ID=$SGE_TASK_ID -DJOB_ID=$JOB_ID -cp ./dist/caf-0.2.jar caf.CorrAttractorFinder $CONF $COMMAND $JOBID $BREAKPOINT $OVLPTH
